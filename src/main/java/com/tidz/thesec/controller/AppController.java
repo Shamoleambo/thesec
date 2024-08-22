3f1901a4873f88837169402f2612d43d5586770f
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-22 09:55:23 -0300

    create register route

--- src/main/java/com/tidz/thesec/controller/AppController.java
+++ src/main/java/com/tidz/thesec/controller/AppController.java
@@ -2,8 +2,10 @@ package com.tidz.thesec.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tidz.thesec.model.User;
 import com.tidz.thesec.repository.UserRepository;
 
 @Controller
@@ -15,6 +17,12 @@ public class AppController {
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
 	}
 
 }
