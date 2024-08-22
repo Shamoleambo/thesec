9a765b9dc0af98c0410401d6d2510bd4ad0e4222
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-22 09:22:22 -0300

    first commit

---
+++ src/main/java/com/tidz/thesec/ThesecApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.thesec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/thesec/ThesecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.thesec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
