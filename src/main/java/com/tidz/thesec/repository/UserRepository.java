3d855d4ffb66fcdcb6530811c92ee56bbf8e4d72
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-23 10:11:03 -0300

    add method findByEmail to the UserRepository interface

--- src/main/java/com/tidz/thesec/repository/UserRepository.java
+++ src/main/java/com/tidz/thesec/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.tidz.thesec.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tidz.thesec.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	public User findByEmail(String email);
 
 }
