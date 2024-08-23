7602c23c5c09df0a5a315abb44f72a677b3ca6f5
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-23 09:52:52 -0300

    create route to create user

--- src/main/java/com/tidz/thesec/controller/AppController.java
+++ src/main/java/com/tidz/thesec/controller/AppController.java
@@ -1,9 +1,11 @@
 package com.tidz.thesec.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tidz.thesec.model.User;
 import com.tidz.thesec.repository.UserRepository;
@@ -23,6 +25,17 @@ public class AppController {
 	public String showRegistrationForm(Model model) {
 		model.addAttribute("user", new User());
 		return "signup_form";
+	}
+
+	@PostMapping("/process_register")
+	public String saveUser(User user) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+
+		this.repo.save(user);
+
+		return "register_user";
 	}
 
 }
