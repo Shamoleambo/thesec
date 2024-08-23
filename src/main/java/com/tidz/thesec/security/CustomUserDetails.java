43607cda6eab432f44233573c3bbbf05c34e1656
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-23 10:05:49 -0300

    create class to represent authentication user

---
+++ src/main/java/com/tidz/thesec/security/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.tidz.thesec.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.tidz.thesec.model.User;
+
+public class CustomUserDetails implements UserDetails {
+
+	private User user;
+
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.user.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public String getFullName() {
+		return this.user.getFirstName() + " " + this.user.getLastName();
+	}
+
+}
